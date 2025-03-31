026d821e5874bfd6a5ff17df3283a5eb87e469c5
Steven Kenny <steveke@umich.edu>
2025-03-31 18:04:06 -0400

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Steven Kenny on 3/31/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
